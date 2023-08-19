1f870977b59453d85faaafa81a3b53cf9c46f1ad
Arthur V. M. Dantas <me@arthurvmdantas.work>
2023-08-19 05:25:06 -0300

    improving code comments/documentation

--- ch3/code/management.js
+++ ch3/code/management.js
@@ -1,10 +1,24 @@
 import * as R from "ramda";
 
 export class Management {
+  /**
+   * Check if the given email belongs to a librarian.
+   *
+   * @param {string} email - The email to check.
+   * @param {object} data - The data object containing librarian information.
+   * @return {boolean} - True if the email belongs to a librarian, false otherwise.
+   */
   static isLibrarian(email, data) {
     return email in R.path(["management", "librariansByEmail"], data);
   }
 
+  /**
+   * Determines if the given email belongs to a VIP member.
+   *
+   * @param {string} email - The email to check.
+   * @param {object} data - The data object containing member information.
+   * @return {boolean} Returns true if the email belongs to a VIP member, otherwise returns false.
+   */
   static isVIPMember(email, data) {
     return (
       R.path(["management", "membersByEmail", email, "isVIP"], data) === true
